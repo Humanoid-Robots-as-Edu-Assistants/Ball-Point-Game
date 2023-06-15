1d76020b74f1f9219a6b90427f740b3df161de53
schgimly <82805904+schgimly@users.noreply.github.com>
2023-06-15 11:30:26 +0200

    add Ball-Point-Game

---
+++ BallPointGame/translations/translation_en_US.ts
@@ -0,0 +1,366 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Hallo</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (1)</name>
+        <message>
+            <source>Hallo</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Danke, dass ihr mit mir gespielt habt! Auf wiedersehen!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Danke, dass ihr mit mir gespielt habt! Auf wiedersehen!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Iteration (1)/Game/Animated Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Stop!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Stop!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Iteration (1)/Game/Say (1)</name>
+        <message>
+            <source>Auf die Plätze. Fertig. Los</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Auf die Plätze. Fertig. Los</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Iteration (1)/Game/Say (2)</name>
+        <message>
+            <source>Stop!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Stop!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Iteration (1)/Planning/Say</name>
+        <message>
+            <source>Noch eine Minute</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Noch eine Minute</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Iteration/Animated Say</name>
+        <message>
+            <source>Hallo</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Perfekt. Super!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Perfekt. Super!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Iteration/Animated Say (1)</name>
+        <message>
+            <source>Perfekt. Super!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Perfekt. Super!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Iteration/Game/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Stop!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Stop!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Iteration/Game/Say (1)</name>
+        <message>
+            <source>Auf die Plätze. Fertig. Los</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Auf die Plätze. Fertig. Los</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Iteration/Game/Say (2)</name>
+        <message>
+            <source>Stop! Wie viele Bälle habt ihr geschafft?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Stop! Wie viele Bälle habt ihr geschafft?</translation>
+        </message>
+        <message>
+            <source>Stop!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Stop!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Iteration/Planning/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Ist das Korrekt?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Ist das Korrekt?</translation>
+        </message>
+        <message>
+            <source>Ok die Zeit startet jetzt!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Ok die Zeit startet jetzt!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Iteration/Planning/Animated Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Iteration/Planning/Choice</name>
+        <message>
+            <source>no/no nao/disagree/I disagree</source>
+            <comment>SpeechReco negative</comment>
+            <translation type="vanished">no/no nao/disagree/I disagree</translation>
+        </message>
+        <message>
+            <source>yes/yes nao/agree/I agree/of course</source>
+            <comment>SpeechReco positive</comment>
+            <translation type="vanished">yes/yes nao/agree/I agree/of course</translation>
+        </message>
+        <message>
+            <source>help/help me/i don't know</source>
+            <comment>SpeechReco help</comment>
+            <translation type="vanished">help/help me/i don't know</translation>
+        </message>
+        <message>
+            <source>exit/stop/quit</source>
+            <comment>SpeechReco exit</comment>
+            <translation type="vanished">exit/stop/quit</translation>
+        </message>
+        <message>
+            <source>repeat/pardon/what/excuse me</source>
+            <comment>SpeechReco repeat</comment>
+            <translation type="vanished">repeat/pardon/what/excuse me</translation>
+        </message>
+        <message>
+            <source>I understood %s. Is that correct?</source>
+            <comment>TTS confirmation</comment>
+            <translation type="vanished">I understood %s. Is that correct?</translation>
+        </message>
+        <message>
+            <source>, / or </source>
+            <comment>TTS enumMarks</comment>
+            <translation type="vanished">, / or </translation>
+        </message>
+        <message>
+            <source>No answer is really expected. /Your answer can be %s. /Your answer can be for example %s. /%s?</source>
+            <comment>TTS helpEnumChoices</comment>
+            <translation type="vanished">No answer is really expected. /Your answer can be %s. /Your answer can be for example %s. /%s?</translation>
+        </message>
+        <message>
+            <source> you can ask me: %s. </source>
+            <comment>TTS helpEnumDefault</comment>
+            <translation type="vanished"> you can ask me: %s. </translation>
+        </message>
+        <message>
+            <source>You can also use my tactile sensor to choose the answer./\Pau=600\ Press the front or the rear of my skull cap to go through all of the available answers. Press then the circle in the middle to validate your choice.</source>
+            <comment>TTS helpTactile</comment>
+            <translation type="vanished">You can also use my tactile sensor to choose the answer./\Pau=600\ Press the front or the rear of my skull cap to go through all of the available answers. Press then the circle in the middle to validate your choice.</translation>
+        </message>
+        <message>
+            <source>I did not understand. </source>
+            <comment>TTS notUnderstood</comment>
+            <translation type="vanished">I did not understand. </translation>
+        </message>
+        <message>
+            <source>No question has been defined, so I cannot repeat it. </source>
+            <comment>TTS noQuestion</comment>
+            <translation type="vanished">No question has been defined, so I cannot repeat it. </translation>
+        </message>
+        <message>
+            <source>There is too much noise here! /Try to talk to me more distinctly. /I can't hear you very well. </source>
+            <comment>TTS notUnderstoodAnims</comment>
+            <translation type="vanished">There is too much noise here! /Try to talk to me more distinctly. /I can't hear you very well. </translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Iteration/Planning/Say</name>
+        <message>
+            <source>Noch eine Minute</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Noch eine Minute</translation>
+        </message>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Iteration/Planning/Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Ist das Korrekt?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Ist das Korrekt?</translation>
+        </message>
+        <message>
+            <source>Ok die Zeit startet jetzt! Plant, wie ihr diese Aufgabe angehen möchtet.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Ok die Zeit startet jetzt! Plant, wie ihr diese Aufgabe angehen möchtet.</translation>
+        </message>
+        <message>
+            <source>Noch eine Minute</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Noch eine Minute</translation>
+        </message>
+        <message>
+            <source>Die Zeit zum Planen startet jetzt.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Die Zeit zum Planen startet jetzt.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Iteration/Retrospektive/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Ihr habt.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Ihr habt.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Iteration/Retrospektive/Animated Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>geschafft.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">geschafft.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Iteration/Retrospektive/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Wie viele Bälle habt ihr geschafft?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Wie viele Bälle habt ihr geschafft?</translation>
+        </message>
+        <message>
+            <source>Ihr habt</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Ihr habt</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Iteration/Retrospektive/Say (1)</name>
+        <message>
+            <source>Ihr habt</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Ihr habt</translation>
+        </message>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>geschätzt</source>
+            <comment>Text</comment>
+            <translation type="obsolete">geschätzt</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Iteration/Retrospektive/Say (2)</name>
+        <message>
+            <source>geschätzt. </source>
+            <comment>Text</comment>
+            <translation type="obsolete">geschätzt. </translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Iteration/Say</name>
+        <message>
+            <source>Hallo</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hallo</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Hallo</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (2)</name>
+        <message>
+            <source>Hallo</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+</TS>
